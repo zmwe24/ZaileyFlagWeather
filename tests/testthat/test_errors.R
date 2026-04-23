6f46ebe64676c21d7287c70b747e97314d0f58a3
Zailey <zmw24@nau.edu>
2026-04-23 15:14:03 -0700

    Delete tests directory

--- tests/testthat/test_errors.R
+++
@@ -1,7 +0,0 @@
-# Checks to see if we get the error message
-
-test_that('Checking for errors', {
-  expect_error(MyFunction(-9,3))
-  expect_error(MyFunction(9,-3))
-  expect_error(MyFunction(0,1))
-})
